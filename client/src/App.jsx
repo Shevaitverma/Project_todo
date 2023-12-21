a701a6d36ab8187d45e9a527809e2f65eae3816f
Shevaitverma <shevaitverma333@gmail.com>
2023-12-21 17:33:56 +0530

    initial checkups and configuration for the new version

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h2>This is root compartment</h2>
+    </>
+  )
+}
+
+export default App
--- server/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE todo;
-
-CREATE TABLE todo_list(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
-);
