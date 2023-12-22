d046ee2b34d9698a68a3359a7c20e0b37ac09064
Shevaitverma <shevaitverma333@gmail.com>
2023-12-22 11:51:47 +0530

    Started implemented frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
+// import { Card } from "./components/card"
+import { LogIn } from "./components/login"
+// import { Register } from "./components/register"
+
+
 function App() {
+
+//   const obj = {
+//     name:"rexxy",
+//     age:24
+//   }
 
   return (
     <>
-      <h2>This is root compartment</h2>
+    <h1 
+    className="bg-green-400 text-black text-center p-2 rounded-lg">
+      My Todo
+    </h1>
+
+    <LogIn />
+    {/* <Register /> */}
+
+{/* 
+    <Card name="Rexxy" myObj={obj}/>
+    <Card />
+    <Card /> */}
+
+
     </>
   )
 }
