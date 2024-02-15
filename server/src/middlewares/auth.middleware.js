dd4e9064e10ef4406b2f1a9f984d038adc6a1418
Shevaitverma <shevaitverma333@gmail.com>
2024-02-15 13:24:03 +0530

    auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,3 +1,28 @@
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
-import {User} from '../models/auth/user.model.js'
+import {User} from '../models/auth/user.model.js';
+import {ApiError} from '../utils/ApiError.js';
+
+export const verifyJwt = asyncHandler(async(req, res, next)=>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if(!token){
+            throw new ApiError(401, "Unauthorized request, Login again !!!")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+
+        if(!user){
+            throw new ApiError(401, "Invalid Access token")
+        }
+
+        req.user = user;
+        next()
+        
+    } catch (error) {
+        throw new ApiError(401, error?.message || "something went wrong during verifying access token!!!")
+    }
+})
