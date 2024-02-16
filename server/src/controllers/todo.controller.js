dc10f018a6ac0af02b739a4618e6570a8e65994b
Shevaitverma <shevaitverma333@gmail.com>
2024-02-16 22:01:52 +0530

    create todo controller

--- server/src/controllers/todo.controller.js
+++ server/src/controllers/todo.controller.js
@@ -1,8 +1,32 @@
 import asyncHandler from "express-async-handler";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js"
+import { Todo } from "../models/app/todo.model.js";
 
-// registeration controller
+// Create a new todo
 const createTodo = asyncHandler(async(req, res)=> {
-    res.send("working")
+    
+    // get data 
+    const {title, description} = req.body;
+
+    if(!title || !description){
+        throw new ApiError(400, "Title and description is required.")
+    }
+
+    // create todo
+    const todo = await Todo.create({
+        title,
+        description,
+        createdBy: req.user._id // we are getting _id from auth middleware if user is logged in.
+    })
+
+    if(!todo){
+        throw new ApiError(400, "Something went wrong, while creating todo.")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,"Todo screated successfully.")
+    )
 })
 
 
