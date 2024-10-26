294f06870168fd66e664e6f4da2d1ea8aa5de27a
Shevaitverma <shevaitverma333@gmail.com>
2024-10-26 19:07:00 +0530

    fixed my proxy issue

--- todo-client/vite.config.js
+++ todo-client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4001/api', // Your backend server
+        changeOrigin: true,               // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove `/api` from the beginning of the path
+      },
+    },
+  },
 })
